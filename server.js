8c2859e04f79ed68f2464cd061a7201bb6438817
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-13 04:35:36 -0600

    separating code for register route, to resolve dependencies we use dependency injection for new register.js file

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const   express = require('express'),
         bodyParser = require('body-parser'),
         cors = require('cors'),
         knex = require('knex'),
-        bcrypt = require('bcrypt-nodejs')
+        bcrypt = require('bcrypt-nodejs'),
+        register = require('./controllers/register')
         
 const pg_db = knex({
     client: 'pg',
@@ -41,32 +42,7 @@ app.post('/signin', (req, res) => {
         .catch(err => res.status(400).json('wrong credentials'));
 });
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    
-    pg_db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                name: name,
-                email: loginEmail[0],
-                joined: new Date()
-            })
-            .then(user => res.json(user[0]))
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'));
-});
+app.post('/register', (req, res) => { register.handleRegister(req, res, pg_db, bcrypt) } ); // nice use of dependency injection
 
 app.get('/profile/:id', (req, res) => {
     let { id } = req.params;
