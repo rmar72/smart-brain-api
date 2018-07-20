f044bb46dbbd7d80d7d42a38f0be2136a4354a63
DESKTOP-Q66EQI4\rs7 <rsolm72@gmail.com>
2018-07-20 04:29:45 -0600

    created new imageurl post route to handle clarifai api call due to reason that this logic exposed the api key when it was on the front end

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require('clarifai');
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: 'e335640338224000ba3d1826e422aedb'
+});
+
+const handleApiCall = (req, res) => {
+    clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input )
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
 const handleImage = (req, res, pg_db) => {
     let { id } = req.body;
     pg_db('users').where('id', '=', id)
@@ -9,4 +24,4 @@ const handleImage = (req, res, pg_db) => {
         .catch(err => res.status(400).json('unable to get entries'));
 }
 
-module.exports = { handleImage }
+module.exports = { handleImage, handleApiCall }
--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, pg_db, b
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, pg_db) } );
 
 app.put('/image',  (req, res) => { image.handleImage(req, res, pg_db) } );
+app.post('/imageURL',  (req, res) => { image.handleApiCall(req, res) } );
 
 app.listen(3007, () => {
     console.log('app is running on port 3007');
